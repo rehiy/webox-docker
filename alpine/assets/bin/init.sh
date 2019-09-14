41940447f8fee81fcbf20620fd90f9ee1a6f64ce
若海 <mail@anrip.com>
2019-09-15 05:45:14 +0800

    use webox intall ssh

--- alpine/assets/bin/init.sh
+++ alpine/assets/bin/init.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 #
 
-source /srv/webox/function
-
 if [ -x /srv/startup ]; then
     /srv/startup init
 fi
 
 if [ ! "$1" == "ssh" ]; then
-    wbx_app_run ssh install
+    /srv/bin/webox ssh install
     exec /usr/sbin/sshd -D
 else
     exec "${@}"
