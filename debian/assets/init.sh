b957b51992482f8243a4f1c9f10495616619a25c
若海 <mail@anrip.com>
2020-04-05 19:48:40 +0800

    修复 自动初始化异常

--- debian/assets/init.sh
+++ debian/assets/init.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
 
+if [ ! -d /var/lib/webox/ready ]; then
+    mkdir -p /var/lib/webox/ready
+    rm -f /srv/app/etc/version
+fi
+
 if [ -x /srv/app/etc/rc.local ]; then
     /srv/app/etc/rc.local
 fi
