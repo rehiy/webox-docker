0ac1a84cacd5f20d285c395c1508a3c77de110aa
若海 <mail@anrip.com>
2020-12-20 03:12:07 +0800

    use sh -e

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -ex
 #
 
 if [ ! -x /usr/bin/dos2unix ]; then
@@ -10,10 +10,11 @@ find rootfs/ -type f -exec dos2unix {} \;
 docker build -t dev/webox .
 sleep 3
 
-docker run --name tmp321 --rm dev/webox &
+docker run --name tmp321 dev/webox &
 sleep 9
 
 docker exec -it tmp321 /bin/sh
+docker rm -f tmp321
 sleep 3
 
 docker rmi -f dev/webox
